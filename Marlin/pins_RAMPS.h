423499fc95fdb735c6837d4e066c9ac827113e77
Samuel Riolo <samuel.riolo@googlemail.com>
2019-03-17 21:07:08 +0100

    using Y-Max pin for BLTouch

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -74,7 +74,9 @@
   #define X_MAX_PIN         2
 #endif
 #define Y_MIN_PIN          14
-#define Y_MAX_PIN          15
+
+// changed by sam pin used for bltouch
+//#define Y_MAX_PIN          15
 #define Z_MIN_PIN          18
 
 // changed by sam
@@ -85,7 +87,8 @@
 // Z Probe (when not Z_MIN_PIN)
 //
 #ifndef Z_MIN_PROBE_PIN
-  #define Z_MIN_PROBE_PIN  32
+  // changed by sam, used for bltouch
+  #define Z_MIN_PROBE_PIN  15//32
 #endif
 
 //
